b9466e6401596dea7cb3e33367c49a22132a99dc
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 06:56:51 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan8/pecahan/IF10K10119906Latihan8Pecahan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan8.pecahan;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Deklarasi Variabel Float.
+ */
+public class IF10K10119906Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        float f;
+        double d;
+        
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        
+    }
+    
+}
